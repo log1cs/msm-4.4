38fe0a232ee5e09000e3bafd199e1e2dfcaae80a
Tuan Anh <tuan73176@gmail.com>
2025-07-20 11:43:10 +0700

    include/linux: import Nokia changes
    Signed-off-by: Tuan Anh <tuan73176@gmail.com>

--- include/linux/hid.h
+++ include/linux/hid.h
@@ -343,7 +343,6 @@ struct hid_item {
 #define HID_GROUP_RMI				0x0100
 #define HID_GROUP_WACOM				0x0101
 #define HID_GROUP_LOGITECH_DJ_DEVICE		0x0102
-#define HID_GROUP_STEAM				0x0103
 
 /*
  * HID protocol status
@@ -379,13 +378,11 @@ struct hid_global {
 
 struct hid_local {
 	unsigned usage[HID_MAX_USAGES]; /* usage array */
-	u8 usage_size[HID_MAX_USAGES]; /* usage size array */
 	unsigned collection_index[HID_MAX_USAGES]; /* collection index array */
 	unsigned usage_index;
 	unsigned usage_minimum;
 	unsigned delimiter_depth;
 	unsigned delimiter_branch;
-	unsigned int usage_page_preceding;
 };
 
 /*
@@ -529,12 +526,10 @@ struct hid_device {							/* device report descriptor */
 	 * battery is non-NULL.
 	 */
 	struct power_supply *battery;
-	__s32 battery_capacity;
 	__s32 battery_min;
 	__s32 battery_max;
 	__s32 battery_report_type;
 	__s32 battery_report_id;
-	bool battery_reported;
 #endif
 
 	unsigned int status;						/* see STAT flags above */
